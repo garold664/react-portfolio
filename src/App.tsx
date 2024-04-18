cc8be2cabdd946e58851d2b55ab92f2e383bec67
garold664 <garold664@gmail.com>
2024-04-18 11:38:40 +0300

    added hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss';
+import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,32 @@
+import styles from './Hero.module.scss';
+
+export default function Hero() {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.heroContainer}>
+        <div className={styles.heroContent}>
+          <h1 className={styles.heroTitle}>
+            Front-End <span className={styles.heroTitleHighlight}>React</span>{' '}
+            Developer 👋
+          </h1>
+          <p className={styles.heroSubtitle}>
+            Hello, my name is{' '}
+            <span className={styles.heroSubtitleHighlight}>
+              Niaz Mukhametzianov
+            </span>
+            . I am a passionate Front-end React Developer based in Kazan, Russia
+          </p>
+        </div>
+        <div className={styles.heroImgContainer}>
+          <div className={styles.heroImgHolder}>
+            <img
+              className={styles.heroImg}
+              src="./assets/images/hero-image.jpg"
+              alt="Hero"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
